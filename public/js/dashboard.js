e76116c1b787d5cb72660b437501d2d79336ba06
saramangelo <saramangelo@gmail.com>
2023-01-23 00:09:28 -0800

    updated button on dashboard

--- public/js/dashboard.js
+++ public/js/dashboard.js
@@ -1,47 +1,47 @@
 const newFormHandler = async (event) => {
   event.preventDefault();
 
-  const title = document.querySelector('#blog-title').value.trim();
-  const contents = document.querySelector('#blog-contents').value.trim();
+  const title = document.querySelector("#blog-title").value.trim();
+  const contents = document.querySelector("#blog-contents").value.trim();
 
   if (title && contents) {
     let input = JSON.stringify({ post_title: title, contents });
     const response = await fetch(`/api/blogs`, {
-      method: 'POST',
+      method: "POST",
       body: input,
       headers: {
-        'Content-Type': 'application/json',
+        "Content-Type": "application/json",
       },
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace("/dashboard");
     } else {
-      alert('Failed to create blog');
+      alert("Failed to create blog");
     }
   }
 };
 
 const delButtonHandler = async (event) => {
-  if (event.target.hasAttribute('data-id')) {
-    const id = event.target.getAttribute('data-id');
+  if (event.target.hasAttribute("data-id")) {
+    const id = event.target.getAttribute("data-id");
 
     const response = await fetch(`/api/blogs/${id}`, {
-      method: 'DELETE',
+      method: "DELETE",
     });
 
     if (response.ok) {
-      document.location.replace('/dashboard');
+      document.location.replace("/dashboard");
     } else {
-      alert('Failed to delete blog');
+      alert("Failed to delete blog");
     }
   }
 };
 
 document
-  .querySelector('.new-blog-form')
-  .addEventListener('submit', newFormHandler);
+  .querySelector(".new-blog-form")
+  .addEventListener("submit", newFormHandler);
 
 document
-  .querySelector('.blog-list')
-  .addEventListener('click', delButtonHandler);
+  .querySelector(".delete-btn")
+  .addEventListener("click", delButtonHandler);
