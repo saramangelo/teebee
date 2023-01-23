51c6c0f5d774331e351177a2a19dd1c910421e75
saramangelo <saramangelo@gmail.com>
2023-01-23 01:04:00 -0800

    successfully sending data back to user

--- controllers/api/blogRoutes.js
+++ controllers/api/blogRoutes.js
@@ -1,8 +1,8 @@
-const router = require('express').Router();
-const { Blog } = require('../../models');
-const withAuth = require('../../utils/auth');
+const router = require("express").Router();
+const { Blog } = require("../../models");
+const withAuth = require("../../utils/auth");
 
-router.post('/', withAuth, async (req, res) => {
+router.post("/", withAuth, async (req, res) => {
   try {
     const newBlog = await Blog.create({
       ...req.body,
@@ -15,20 +15,18 @@ router.post('/', withAuth, async (req, res) => {
   }
 });
 
-router.get('/:id', async (req, res) => {
+router.get("/:id", async (req, res) => {
   try {
-    const blogData = await Blog.findOne({ where: { id: req.params.id }
-    });
+    const blogData = await Blog.findOne({ where: { id: req.params.id } });
     const blog = blogData.get({ plain: true });
 
-    console.log(blog);
-  // res.status(200).json(blogData);
+    res.status(200).json(blog);
   } catch (err) {
     res.status(500).json(err);
   }
-})
+});
 
-router.delete('/:id', withAuth, async (req, res) => {
+router.delete("/:id", withAuth, async (req, res) => {
   try {
     const blogData = await Blog.destroy({
       where: {
@@ -38,7 +36,7 @@ router.delete('/:id', withAuth, async (req, res) => {
     });
 
     if (!blogData) {
-      res.status(404).json({ message: 'No blog found with this id!' });
+      res.status(404).json({ message: "No blog found with this id!" });
       return;
     }
 
