7e2a9457f246159877d022de6a3a238c6c42b9a8
saramangelo <saramangelo@gmail.com>
2023-01-27 09:56:46 -0800

    added edit/delete buttons on comments, need to add functionality

--- public/js/comments.js
+++ public/js/comments.js
@@ -30,3 +30,78 @@ const submitComment = async () => {
 };
 
 document.querySelector('#comment-submit-button').addEventListener('click', submitComment);
+
+// create/edit/delete comments
+
+// const title = document.querySelector("#blog-title");
+// const contents = document.querySelector("#blog-contents");
+
+// let post_id = 0;
+// console.log(post_id);
+
+// const newFormHandler = async (event) => {
+//   event.preventDefault();
+
+//   let post_title = title.value.trim();
+//   let info = contents.value.trim();
+
+//   if (title && contents) {
+//     let response;
+//     let input = { post_title, contents: info };
+
+//     if (post_id === 0) {
+//       response = await fetch(`/api/blogs`, {
+//         method: "POST",
+//         body: JSON.stringify(input),
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
+//     } else {
+//       let updated_post = { ...input, id: post_id };
+
+//       response = await fetch(`/api/blogs`, {
+//         method: "PUT",
+//         body: JSON.stringify(updated_post),
+//         headers: {
+//           "Content-Type": "application/json",
+//         },
+//       });
+//     }
+
+//     if (response.ok) {
+//       document.location.replace("/dashboard");
+//     } else {
+//       alert("Failed to create blog");
+//     }
+//   }
+// };
+
+// const delButtonHandler = async (event) => {
+//   if (event.target.matches(".delete-btn")) {
+//     console.log(event.target)
+//   if (event.target.hasAttribute("data-id")) {
+//     const id = event.target.getAttribute("data-id");
+
+//     const response = await fetch(`/api/blogs/${id}`, {
+//       method: "DELETE",
+//     });
+
+//     if (response.ok) {
+//       document.location.replace("/dashboard");
+//     } else {
+//       alert("Failed to delete blog");
+//     }
+//   }
+// }
+// };
+
+// let edtBtn = document.querySelectorAll('.edit-btn')
+// for (let i = 0; i < edtBtn.length; i++) {
+//   edtBtn[i].addEventListener("click", editButtonHandler);
+// };
+
+// let delBtn = document.querySelectorAll('.delete-btn')
+// for (let i = 0; i < delBtn.length; i++) {
+//   delBtn[i].addEventListener("click", delButtonHandler);
+// };
